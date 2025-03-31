9cd30320371bd7eea9c80bb1a66ce0387359b3c0
gtak <gtak@itu.dk>
2025-03-31 12:23:39 +0200

    Description of the image folder

--- data/datasort.py
+++ data/datasort.py
@@ -1,5 +1,5 @@
 #Lets make it a bit easier for us to look through the metadata csv
-
+#The images folder should only contain the images of the lesions without subfolders.
 import pandas as pd
 df=pd.read_csv('data\metadata.csv',header=0)
 df=df.sort_values(by=['patient_id'])
