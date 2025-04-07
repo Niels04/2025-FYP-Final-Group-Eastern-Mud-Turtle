727d36630848c4c6756098d193e448cad2b7d97d
maiagh <zamolxia@gmail.com>
2025-04-07 08:32:06 +0200

    progress on testing and describing

--- util/extract_features_examples.py
+++ util/extract_features_examples.py
@@ -415,31 +415,34 @@ def find_midpoint_v4(mask):
 
 def cut_mask(mask):
     
-    col_sums = np.sum(mask, axis=0)
-    row_sums = np.sum(mask, axis=1)
+    # input is numpy array mask
+    col_sums = np.sum(mask, axis=0)     # sums up the values between 0 and 1
+    row_sums = np.sum(mask, axis=1)     # shows if any row or column contains anything but 0s
 
-    active_cols = []
-    for index, col_sum in enumerate(col_sums):
-        if col_sum != 0:
+    active_cols = []        # lists all the columns where there is no lesion
+    for index, col_sum in enumerate(col_sums):  # takes all columns
+        if col_sum != 0:                        # if the full column is 0, it's not added to the list
             active_cols.append(index)
 
-    active_rows = []
+    active_rows = []        # analog for rows
     for index, row_sum in enumerate(row_sums):
         if row_sum != 0:
             active_rows.append(index)
 
+    # taking the bordering rows and columns of the mask (excluding the black edges where there is nothing)
     col_min = active_cols[0]
     col_max = active_cols[-1]
     row_min = active_rows[0]
     row_max = active_rows[-1]
 
+    # saving the new mask
     cut_mask_ = mask[row_min:row_max+1, col_min:col_max+1]
 
     return cut_mask_
 
 def cut_im_by_mask(image, mask):
     
-
+    # same as previous function
     col_sums = np.sum(mask, axis=0)
     row_sums = np.sum(mask, axis=1)
 
@@ -458,33 +461,41 @@ def cut_im_by_mask(image, mask):
     row_min = active_rows[0]
     row_max = active_rows[-1]
 
+    #except the cutting is applied to the image itself and not the mask
     cut_image = image[row_min:row_max+1, col_min:col_max+1]
 
     return cut_image
 
 def find_midpoint_v1(image):
     
-    row_mid = image.shape[0] / 2
-    col_mid = image.shape[1] / 2
+    row_mid = image.shape[0] / 2     # nr of the middle row
+    col_mid = image.shape[1] / 2     # nr of the middle column
     return row_mid, col_mid
 
 def asymmetry(mask):
     
 
-    row_mid, col_mid = find_midpoint_v1(mask)
+    row_mid, col_mid = find_midpoint_v1(mask)           # uses the previous function
 
+    # slicing into 4 different halves. We will calculate symmetry on both halves
+    # ceil and floor are used in case the outputs of the midpoint functions have decimals
     upper_half = mask[:ceil(row_mid), :]
     lower_half = mask[floor(row_mid):, :]
     left_half = mask[:, :ceil(col_mid)]
     right_half = mask[:, floor(col_mid):]
 
+    # flip one of the complementary halves of each pair (one horizontal half, one vertical half)
     flipped_lower = np.flip(lower_half, axis=0)
     flipped_right = np.flip(right_half, axis=1)
 
+    # using the xor logic, makes a binary array of all pixels in both symmetry pairs
     hori_xor_area = np.logical_xor(upper_half, flipped_lower)
     vert_xor_area = np.logical_xor(left_half, flipped_right)
 
+    # to calculate ratio, we need to know how many pixels there are in total
     total_pxls = np.sum(mask)
+    # the sum together how many true values there are
+    # keeping note of xor logic => the more ASYMMETRY the higher the number
     hori_asymmetry_pxls = np.sum(hori_xor_area)
     vert_asymmetry_pxls = np.sum(vert_xor_area)
 
