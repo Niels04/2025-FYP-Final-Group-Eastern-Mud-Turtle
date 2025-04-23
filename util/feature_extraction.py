1d497c19fbdc5c1443aabfc5c9c22c7f7a560d51
vinp505 <pirasvincenzo05@gmail.com>
2025-04-23 12:24:38 +0200

    Merged two extractor functions into one, which works for all cases (both training and testing scenarios)

--- util/feature_extraction.py
+++ util/feature_extraction.py
@@ -15,98 +15,106 @@ mask_dir = '../data/lesion_masks/'
 metadata_dir = "../data/metadata.csv"
 features_dir = "../data/features.csv"
 
-def training_extract(img_dir, mask_dir, metadata_dir, features_dir, base_model= True) -> None:
-    """Auxiliary python code that, given a set of lesion images with relative masks and metadata file,
-    outputs in a csv the relevant information about the lesion, such as patient id and lesion id, 
-    together with the extracted features, thanks to the imported functions.
-    A binary indicator about wether or not the lesion is a melanoma is also saved.
-    Intended to be used to load test data.
+# def training_extract(img_dir, mask_dir, metadata_dir, features_dir, base_model= True) -> None:
+#     """Auxiliary python code that, given a set of lesion images with relative masks and metadata file,
+#     outputs in a csv the relevant information about the lesion, such as patient id and lesion id, 
+#     together with the extracted features, thanks to the imported functions.
+#     A binary indicator about wether or not the lesion is a melanoma is also saved.
+#     Intended to be used to load test data.
 
-    :param img_dir: The directory of the lesion images to be processed.
-    :param mask_dir: The directory of the relative mask images.
-    :param metadata_dir: The directory of the csv file containing information on the lesions.
-    :param features_dir: The directory in which the csv file containing extracted features will be saved
-    :param base_model: Boolean value to indicate if only the base features will be extracted, defaulted to True.
+#     :param img_dir: The directory of the lesion images to be processed.
+#     :param mask_dir: The directory of the relative mask images.
+#     :param metadata_dir: The directory of the csv file containing information on the lesions.
+#     :param features_dir: The directory in which the csv file containing extracted features will be saved
+#     :param base_model: Boolean value to indicate if only the base features will be extracted, defaulted to True.
 
-    """
-    # read the relevant csv files
-    md = pd.read_csv(metadata_dir)
-    cd = pd.read_csv(features_dir)
+#     """
+#     # read the relevant csv files
+#     md = pd.read_csv(metadata_dir)
+#     cd = pd.read_csv(features_dir)
 
-    # appropriately name columns
-    if base_model:
-        cd.columns= ['img_id', 'patient_id', 'lesion_id', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score', 'true_melanoma_label']
+#     # appropriately name columns
+#     if base_model:
+#         cd.columns= ['img_id', 'patient_id', 'lesion_id', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score', 'true_melanoma_label']
     
-    else:
-        cd.columns= ['img_id', 'patient_ID', 'lesion_ID', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score', 'fBV_score', 'fCHEESE_score', 'true_melanoma_label']
+#     else:
+#         cd.columns= ['img_id', 'patient_ID', 'lesion_ID', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score', 'fBV_score', 'fCHEESE_score', 'true_melanoma_label']
 
-    # print(len(md[md['diagnostic'] == 'MEL']) == len(cd[cd['true_melanoma_label'] == 1]))
+#     # print(len(md[md['diagnostic'] == 'MEL']) == len(cd[cd['true_melanoma_label'] == 1]))
 
-    # for idx, row in cd[cd['true_melanoma_label'] == 1].iterrows():
-    #     key = row['img_id']
+#     # for idx, row in cd[cd['true_melanoma_label'] == 1].iterrows():
+#     #     key = row['img_id']
         
-    #     # Find matching rows in df2
-    #     print(md[md['img_id'] == key]['diagnostic'].values[0])
+#     #     # Find matching rows in df2
+#     #     print(md[md['img_id'] == key]['diagnostic'].values[0])
 
-    # load up the images and relative masks
-    data_loader = IDL(img_dir, mask_dir)
+#     # load up the images and relative masks
+#     data_loader = IDL(img_dir, mask_dir)
 
-    # iterate through the pairs
-    for img_rgb, img_gray, mask, name in tqdm(data_loader):
+#     # iterate through the pairs
+#     for img_rgb, img_gray, mask, name in tqdm(data_loader):
 
-        # get patient_id and lesion_id from the filename - ALTERNATIVE: get them from metadata csv
-        name_split = name.split('_')
-        patient_ID = '_'.join(name_split[:2])
-        lesion_ID = int(name_split[2])
+#         # get patient_id and lesion_id from the filename - ALTERNATIVE: get them from metadata csv
+#         name_split = name.split('_')
+#         patient_ID = '_'.join(name_split[:2])
+#         lesion_ID = int(name_split[2])
 
-        # get full ID
-        pat_les_ID = '_'.join(name_split[:3])
+#         # get full ID
+#         pat_les_ID = '_'.join(name_split[:3])
         
-        # remove the hair from the image
-        # _, _, img_rgb_nH = rH(img_rgb, img_gray)
+#         # remove the hair from the image
+#         # _, _, img_rgb_nH = rH(img_rgb, img_gray)
 
 
-        # extract the features with the proper functions
+#         # extract the features with the proper functions
 
-        fA_score = fA_extractor(mask)                      # asymmetry - roundness of image
-        fB_score = fB_extractor(mask)                      # border irregularity - compactness of image
-        fC_score = fC_extractor(img_rgb, mask)             # color - amount of different colors in image
+#         fA_score = fA_extractor(mask)                      # asymmetry - roundness of image
+#         fB_score = fB_extractor(mask)                      # border irregularity - compactness of image
+#         fC_score = fC_extractor(img_rgb, mask)             # color - amount of different colors in image
 
-        if not base_model:
-            fBV_score = fBV_extractor(img_rgb, mask)       # blue veil - amount of blue-ish pixels in lesion
-            fCHEESE_score = fCHEESE_extractor(mask)        # cheese - number of clusters in the mask of the lesion
+#         if not base_model:
+#             fBV_score = fBV_extractor(img_rgb, mask)       # blue veil - amount of blue-ish pixels in lesion
+#             fCHEESE_score = fCHEESE_extractor(mask)        # cheese - number of clusters in the mask of the lesion
 
 
-        # get the actual diagnosis for the lesion, and convert it in a 
-        # binary value to indicate if it is melanoma or not
-        diagnosis = md[md['img_id'] == name]['diagnostic'].values[0]
-        true_label = 1 if diagnosis == 'MEL' else 0
+#         # get the actual diagnosis for the lesion, and convert it in a 
+#         # binary value to indicate if it is melanoma or not
+#         diagnosis = md[md['img_id'] == name]['diagnostic'].values[0]
+#         true_label = 1 if diagnosis == 'MEL' else 0
 
-        # create the new column for the final csv file
-        if base_model:
-            datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score, true_label]
+#         # create the new column for the final csv file
+#         if base_model:
+#             datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score, true_label]
         
-        else:
-            datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score, fBV_score, fCHEESE_score, true_label]
+#         else:
+#             datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score, fBV_score, fCHEESE_score, true_label]
 
-        cd.loc[len(cd)] = datapoint
+#         cd.loc[len(cd)] = datapoint
 
-    # save the updated classified.csv
-    cd.to_csv(features_dir)
+#     # save the updated classified.csv
+#     cd.to_csv(features_dir)
 
-    # TEMPORARY: how many name mismatches (48, out of 2000+)
-    print(f'Out of {len(data_loader)} image - mask pairs, {data_loader.lost} were lost due to name discrepancies.')
+#     # TEMPORARY: how many name mismatches (48, out of 2000+)
+#     print(f'Out of {len(data_loader)} image - mask pairs, {data_loader.lost} were lost due to name discrepancies.')
 
 
 # hepler function for main python script
-def extract(img_dir, mask_dir= None, base_model= True) -> pd.DataFrame:
-    """Function to create and return a Pandas data frame that stores information about all image - maks pairs in the given directory.
+def extract(img_dir, mask_dir= None, metadata_dir= None, features_dir= None, base_model= True) -> pd.DataFrame:
+    """Function to create, return and optionally save a Pandas data frame that stores information about 
+    all image-maks pairs in the given directory.
     If only one directory is specified, the function assumes that both masks and images are found in it.
-    If two directories are given, the first one will be treated as the image directory, and the second one as the mask directory.
-    The data frame contains, for each image, name of the file, patient and lesion id, value for all extracted features.
+    By default, no metadata file will be loaded, the data frame will not be saved and only the base
+    features will be extracted.
+    To modify that, refer to the parameter documentation below.
     
     :param img_dir: The directory of the lesion images to be processed.
     :param mask_dir: The directory of the mask images, defaulted to None.
+    :param metadata_dir: The directory of the metadata csv file containing the true melanoma label for the images,
+                         with the file name under the column 'img_id', and the true label under the column 'diagnostic'.
+                         Default is None, in which case no metadata file will be read and the resulting csv will not
+                         contain the 'true_melanoma_label' column.
+    :param features_dir: The directory in which to save the resulting data frame as a csv file.
+                         Default is None, in which case the data frame will not be saved, but simply returned. 
     :param base_model: Boolean value to indicate if only the base features will be extracted, defaulted to True.
     
     :return: Pandas data frame with name of the file, patient and lesion id, value for each extracted feature, 
@@ -120,9 +128,14 @@ def extract(img_dir, mask_dir= None, base_model= True) -> pd.DataFrame:
     else:
         data_loader = IDL(img_dir, img_dir)
 
+
+    # set up data frames according to parameters
+
+    if metadata_dir:
+        md = pd.read_csv(metadata_dir)
     
-    # set up data frame to store results
     cd = pd.DataFrame()
+
 
     # set up rows array
     rows = []
@@ -151,24 +164,44 @@ def extract(img_dir, mask_dir= None, base_model= True) -> pd.DataFrame:
 
 
         # create the new column for the final csv file
-        # create the new column for the final csv file
-        if base_model:
-            datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score]
+
+        datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score]
         
-        else:
-            datapoint = [name, patient_ID, lesion_ID, pat_les_ID, fA_score, fB_score, fC_score, fBV_score, fCHEESE_score]
+        if not base_model:
+            datapoint += [fBV_score, fCHEESE_score]
+        
+        # get true melanoma label if metadata file is given
+        if metadata_dir:
+
+            # binary value to indicate if it is melanoma or not
+            diagnosis = md[md['img_id'] == name]['diagnostic'].values[0]
+            true_label = 1 if diagnosis == 'MEL' else 0
+
+            # add label to datapoint
+            datapoint.append(true_label)
+
         rows.append(datapoint)
     
     # add rows to data frame
     cd = pd.concat([cd, pd.DataFrame(rows)])
 
-    # appropriately name columns
-    if base_model:
-        cd.columns= ['img_id', 'patient_id', 'lesion_id', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score']
-    
-    else:
-        cd.columns= ['img_id', 'patient_id', 'lesion_id', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score', 'fBV_score', 'fCHEESE_score']
 
+    # appropriately name columns
+
+    col_names = ['img_id', 'patient_id', 'lesion_id', 'pat_les_ID', 'fA_score', 'fB_score', 'fC_score']
+
+    if not base_model:
+        col_names += ['fBV_score', 'fCHEESE_score']
+
+    if metadata_dir:
+        col_names.append('true_melanoma_label')
+
+    cd.columns = col_names
+
+    # save df if specified
+    if features_dir:
+        cd.to_csv(features_dir)
+    
     return cd
 
 
